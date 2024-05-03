d9ece995556342f1b67b37810113ee152469ca88
juliangarciaarellano <168446011+juliangarciaarellano@users.noreply.github.com>
2024-05-03 09:57:31 -0400

    Sampling integers in c

--- Math.c
+++ Math.c
@@ -1,2 +1,15 @@
+#include<stdio.h>
 
-hello
+int main(){
+
+    // %d allows for printing of an integer value - 
+    printf("%d", 3+3);
+
+    printf("\n"); // leaves a break
+
+
+    // Calling on a variable 
+    int value = 3+3+3; // Iddentifying varibale as an integer
+    printf("%d", value);
+
+}
