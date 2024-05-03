f902a1803c23db049a43641b59c3a4a56511e759
juliangarciaarellano <168446011+juliangarciaarellano@users.noreply.github.com>
2024-05-03 10:07:30 -0400

    including powers using math.h

--- Math.c
+++ Math.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include<stdio.h> //general c library
+#include<math.h> //allows for powers
 
 int main(){
 
@@ -11,5 +12,14 @@ int main(){
     // Calling on a variable 
     int value = 3+3+3; // Iddentifying varibale as an integer
     printf("%d", value);
+
+    printf("\n"); // leaves a break
+
+    // Inserting variables into an equation
+
+    int n = 5;
+
+
+    printf ("%d", (int) pow (n, 2));
 
 }
